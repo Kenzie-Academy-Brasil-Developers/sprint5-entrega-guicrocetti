f8a342d7c3f51ea73dfd0a95f22c349c6fdfb90a
guicrocetti <crocetti@outlook.com>
2022-08-17 16:52:47 -0300

    init

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,35 +1,25 @@
+// import "reflect-metadata"
 import { DataSource } from "typeorm";
+import { User } from "./entities/User";
 import "dotenv/config";
 
-/* Explicando código abaixo:
-  * Estamos utilizando um ternário para controlar a configuração
-    do DataSource do typeorm.
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PWD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: [User],
+  migrations: ["src/migrations/*.ts"],
+});
 
-  * Utilizamos uma variavel de ambiente para controlar isso pois
-    para conseguir manipular mesmo em um ambiente de produção.
-
-  * O banco em que fazemos os testes é o SQLite e precisamos 
-    das configurações pré definidas abaixo para que os testes executem
-    de forma correta.
-*/
-
-export const AppDataSource =
-  process.env.NODE_ENV === "test"
-    ? new DataSource({
-        type: "sqlite",
-        database: ":memory:",
-        entities: ["src/entities/*.ts"],
-        synchronize: true,
-      })
-    : new DataSource({
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        synchronize: false,
-        logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source Initialized");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source Initialization"), err;
+  });
